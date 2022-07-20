b8f94f5758d0a448dfd565c67e315998f7160377
Lucas Genari <oligena@hotmail.com>
2022-07-19 22:45:56 -0300

    mysql throwing error

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Node / Express</h1>')
+const sql = `INSERT INTO people(name) values('Lucas')`
+connection.query(sql)
+connection.end()
+
+
+app.get('/', (req,res) => {
+    res.send('<h1>Full Cycle</h1>')
 })
 
-app.listen(port, () => {
-    console.log(`Rodando na porta ${port}`)
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
 })
